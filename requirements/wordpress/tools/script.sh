3b79647a51e4c8ed1c417347e91a12d8f433d839
ZoktrFall <shahavetgnum@gmail.com>
2024-01-11 01:09:07 +0400

    Wordpress ready

--- requirements/wordpress/tools/script.sh
+++ requirements/wordpress/tools/script.sh
@@ -1,40 +1,29 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-chmod +x wp-cli.phar 
-mv wp-cli.phar /usr/local/bin/wp
-
-
 mkdir -p /var/www/html
 cd /var/www/html
 
-rm -rf *
+if [ ! -f ./wp-config.php  ]; then
+    rm -rf *
+    wp core download --allow-root
 
+    sed -i "s/username_here/ZoktrFall/g" wp-config-sample.php
+	sed -i "s/password_here/Xxx123/g" wp-config-sample.php
+	sed -i "s/localhost/mariadb/g" wp-config-sample.php
+	sed -i "s/database_name_here/MyDB/g" wp-config-sample.php
+    cp wp-config-sample.php wp-config.php
+    rm -rf wp-config-sample.php
 
-wp core download --allow-root
-
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-if [ -f /wp-config.php ]; then
-    mv /wp-config.php /var/www/html/wp-config.php
+    wp core install --url=aafrikya.42.fr/ --title=Mytitle --admin_user=Zoktr --admin_password=Xxx123 --admin_email=TestOper@gmail.com --skip-email --allow-root
+    wp user create ZoktrFall zoktr@42.com --role=author --user_pass=Xxx456 --allow-root
+    wp theme install astra --activate --allow-root
+    # wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
 fi
 
-# sed -i -r "s/db1/MyDB/1"   wp-config.php
-# sed -i -r "s/user/ZoktrFall/1"  wp-config.php
-# sed -i -r "s/pwd/Xxx123/1"    wp-config.php
-# sed -i -r "s/localhost/mariadb/1"    wp-config.php
-
-wp core install --url=aafrikya.42.fr/ --title=Mytitle --admin_user=Zoktr --admin_password=Xxx123 --admin_email=TestOper@gmail.com --skip-email --allow-root
-wp user create ZoktrFall zoktr@42.com --role=author --user_pass=Xxx456 --allow-root
-wp theme install astra --activate --allow-root
-wp plugin install redis-cache --activate --allow-root
-wp plugin update --all --allow-root
-
- 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-mkdir /run/php
+sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+mkdir -p /run/php
 
 # wp redis enable --allow-root
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm8.2 -F
