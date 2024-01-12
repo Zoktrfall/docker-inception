6ecc0f113052aab15faba287c79c738bb5154d66
ZoktrFall <afrikyanarman24@gmail.com>
2024-01-12 21:35:33 +0400

    Mandatory part finish?

--- requirements/mariadb/tools/script.sh
+++ requirements/mariadb/tools/script.sh
@@ -2,13 +2,17 @@
 
 service mysql start 
 
-echo "CREATE DATABASE IF NOT EXISTS MyDB ;" > db1.sql
+echo "CREATE DATABASE IF NOT EXISTS mydb ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS 'ZoktrFall'@'%' IDENTIFIED BY 'Xxx123' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON MyDB.* TO 'ZoktrFall'@'%' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON mydb.* TO 'ZoktrFall'@'%' ;" >> db1.sql
 # echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
+
+cd /etc/mysql
+echo "[mysqld]" >> my.cnf
+echo "lower_case_table_names=1" >> my.cnf
 
 sleep 2
 kill $(cat /var/run/mysqld/mysqld.pid)
--- requirements/wordpress/tools/script.sh
+++ requirements/wordpress/tools/script.sh
@@ -10,17 +10,17 @@ if [ ! -f ./wp-config.php  ]; then
     sed -i "s/username_here/ZoktrFall/g" wp-config-sample.php
 	sed -i "s/password_here/Xxx123/g" wp-config-sample.php
 	sed -i "s/localhost/mariadb/g" wp-config-sample.php
-	sed -i "s/database_name_here/MyDB/g" wp-config-sample.php
+	sed -i "s/database_name_here/mydb/g" wp-config-sample.php
     cp wp-config-sample.php wp-config.php
-    rm -rf wp-config-sample.php
 
     wp core install --url=aafrikya.42.fr/ --title=Mytitle --admin_user=Zoktr --admin_password=Xxx123 --admin_email=TestOper@gmail.com --skip-email --allow-root
     wp user create ZoktrFall zoktr@42.com --role=author --user_pass=Xxx456 --allow-root
-    wp theme install astra --activate --allow-root
+    wp theme update --all --allow-root
     # wp plugin install redis-cache --activate --allow-root
     wp plugin update --all --allow-root
 fi
 
+wp core update --allow-root
 # wp redis enable --allow-root
 
 /usr/sbin/php-fpm8.2 -F
